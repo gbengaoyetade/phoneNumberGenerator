b7a31f087b0608d2d653a71725d63ce4f88a23bf
gbengaPS <ioyetade@gmail.com>
2018-11-25 18:13:15 +0100

    reconfigure app - add route for generate numbers

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
+const { NumbersController } = require('./controllers');
 
 router.get('/numbers', () => {});
+router.post('/numbers/generate', NumbersController.generateNumbers);
 
 module.exports = router;
--- src/server.js
+++ src/server.js
@@ -5,18 +5,8 @@ const routes = require('./routes');
 const app = express();
 
 app.use(express.json());
-
-app.get('/', (req, res) => {
-  res.send({ message: 'Welcome to phone number generator api' });
-});
+app.use(express.urlencoded());
 
 app.use('/api/v1', routes);
 
-const port = process.env.PORT || 8080;
-if (require.main === module) {
-  app.listen(port, (error) => {
-    if (!error) {
-      console.log(`Listening on port ${port}`);
-    }
-  });
-}
+module.exports = app;
