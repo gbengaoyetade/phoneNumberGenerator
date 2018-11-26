1d6758e456154a79c25f4b1a24ddb7406e838b19
gbengaPS <ioyetade@gmail.com>
2018-11-26 04:00:00 +0100

    implement get numbers - create compare numbers function

--- src/controllers/numbersController.js
+++ src/controllers/numbersController.js
@@ -1,4 +1,10 @@
-const { generateNumbers, MAX_COUNT } = require('../lib');
+const fs = require('fs');
+const {
+  generateNumbers,
+  compareNumbers,
+  MAX_COUNT,
+  FILE_PATH,
+} = require('../lib');
 
 class NumbersController {
   static async generateNumbers(req, res) {
@@ -10,6 +16,36 @@ class NumbersController {
         numbersLength > 1 ? 'numbers' : 'number'
       } generated`,
       numbers,
+    });
+  }
+
+  static async getNumbers(req, res) {
+    const { statsOnly } = req.query;
+    fs.readFile(FILE_PATH, 'utf8', (err, data) => {
+      if (err) {
+        res
+          .status(500)
+          .send({ error: 'There was an error reading database file' });
+      } else {
+        const numbersArray = data.split(',').sort(compareNumbers);
+        let length;
+        // This conditional was added for when the file is empty
+        if (numbersArray[0] === '') {
+          length = 0;
+        } else {
+          length = numbersArray.length; // eslint-disable-line
+        }
+        const responseObject = {
+          length,
+          max: numbersArray[length - 1] || '',
+          min: numbersArray[0],
+          numbers: numbersArray,
+        };
+        if (statsOnly) {
+          delete responseObject.numbers;
+        }
+        res.send(responseObject);
+      }
     });
   }
 }
