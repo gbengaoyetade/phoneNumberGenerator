4ada5b1634b9b3dfb2c1d062bfeb8e2d1c1593c6
gbengaPS <ioyetade@gmail.com>
2018-11-29 05:21:56 +0100

    add tests to frontend app

--- src/client/components/tests/App.spec.js
+++ src/client/components/tests/App.spec.js
@@ -1,13 +1,28 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import * as axios from 'axios';
 import App from '../App';
 
 jest.mock('axios');
-axios.get.mockImplementation(() => Promise.resolve({ data: {} }));
+axios.get.mockImplementation(() => Promise.resolve({ data: { numbers: ['234'] } }));
+const wrapper = mount(<App />);
 describe('App', () => {
   it('should match snapshot', () => {
-    const wrapper = shallow(<App />);
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should set state when handleChange is called', () => {
+    const event = { target: { value: '34' }, preventDefault: jest.fn() };
+    wrapper.instance().handleChange(event);
+    const state = wrapper.state();
+    expect(state.numberInput).toBe(event.target.value);
+  });
+
+  it('should set state when handleSubmit is called', () => {
+    const event = { preventDefault: jest.fn() };
+    wrapper.instance().handleSubmit(event);
+    wrapper.setState({ numberInput: '90' });
+    const state = wrapper.state();
+    expect(state.numbersFetched).toBe(false);
   });
 });
